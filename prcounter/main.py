a1373a01dbee4d340960c5b7edcf2a709cdc4063
Masashi SAKAI <masashi.sakai1986@gmail.com>
2018-08-27 08:33:39 +0900

    コマンドライン引数を読み込むように修正

--- prcounter/main.py
+++ prcounter/main.py
@@ -1,6 +1,7 @@
+import argparse
 import sys
 from datetime import datetime
-from logging import getLogger, StreamHandler, Formatter, DEBUG
+from logging import getLogger, NullHandler, FileHandler, Formatter, DEBUG
 
 import yaml
 from github import Github
@@ -10,29 +11,46 @@ import prcounter.database
 from prcounter.aggregator import GitHubAggregator
 
 
-def setup_logger():
-    formatter = Formatter('%(asctime)s %(levelname)s %(message)s')
-
-    handler = StreamHandler()
-    handler.setLevel(DEBUG)
-    handler.setFormatter(formatter)
+def setup_logger(path=None):
+    handler = NullHandler()
+    if path is not None:
+        formatter = Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler = FileHandler(path)
+        handler.setLevel(DEBUG)
+        handler.setFormatter(formatter)
     logger = getLogger('prcounter')
     logger.setLevel(DEBUG)
     logger.addHandler(handler)
 
 
 def main():
-    setup_logger()
+    parser = argparse.ArgumentParser()
 
-    conf_path = 'etc/config.yaml'
+    parser.add_argument('--config', '-c',
+                        default='etc/config.yaml',
+                        help='設定ファイルのパス')
+    parser.add_argument('--local',
+                        action = 'store_true',
+                        help='GitHubからデータの収集を行わず、ローカルDBを使用する')
+    parser.add_argument('--startdate', '-s',
+                        default='2018-01-01',  # FIXME
+                        help='集計開始日 YYYY-MM-DD')
+    parser.add_argument('--enddate', '-e',
+                        default='2018-03-31',  # FIXME
+                        help='集計終了日 YYYY-MM-DD')
 
-    with open(conf_path) as yaml_file:
+    args = parser.parse_args()
+
+    with open(args.config) as yaml_file:
         config = yaml.load(yaml_file)
 
-    # TODO argparseで実行時に読み込む
-    local = True
-    startdate = '2018-01-01'
-    enddate = '2018-03-31'
+    setup_logger(config['application']['log_path'])
+
+    # バリデーション
+    startdate = datetime.strptime(args.startdate, '%Y-%m-%d')
+    enddate = datetime.strptime(args.enddate, '%Y-%m-%d')
+    assert startdate <= enddate
+
     users = config['github']['users']
 
     db = prcounter.database.Database(
@@ -42,7 +60,7 @@ def main():
     db.create_tables()
 
     # GitHubからデータを収集して, DBを更新する
-    if not local:
+    if not args.local:
         g = Github(
             base_url=config['github']['base_url'],
             login_or_token=config['github']['token'])
@@ -65,7 +83,7 @@ def main():
                   file=sys.stderr)
 
     # 集計結果の表示
-    result = db.count(startdate, enddate, users)
+    result = db.count(args.startdate, args.enddate, users)
 
     db.close()
 
